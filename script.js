753c1476bff722f6ede34af880dd4783408a6652
Alexander Wassbjer <alexanderwassbjer@outlook.com>
2023-08-15 11:18:10 +0200

    Fix indent

--- script.js
+++ script.js
@@ -1,92 +1,69 @@
 var prevUrl = null;
-      var currentPage = location.href;
+var currentPage = location.href;
 
-      setInterval(function () {
-        var iframe = 
-document.getElementById("kalender");
-        
-iframe.contentWindow.postMessage("getLocationHref", 
-iframe.src);
-      }, 1000);
+setInterval(function () {
+  var iframe = document.getElementById("kalender");
 
-      setInterval(function () {
-        if (currentPage != location.href) {
-          currentPage = location.href;
-          window.scrollTo({ top: 0 });
-        }
-      }, 100);
+  iframe.contentWindow.postMessage("getLocationHref", iframe.src);
+}, 1000);
 
-      window.addEventListener("message", 
-(event) => {
-        if (
-          event.origin !== 
-"https://dykarna.plingot.com" &&
-          event.origin !== 
-"https://dykarna.plingot.com" &&
-          event.origin !== 
-"https://dykarna.plingot.com" &&
-          event.origin !== 
-"https://dykarna.plingot.com"
-        ) {
-          return;
-        }
+setInterval(function () {
+  if (currentPage != location.href) {
+    currentPage = location.href;
+    window.scrollTo({ top: 0 });
+  }
+}, 100);
 
-        const iframeLocationHref = 
-event.data.href;
-        if (iframeLocationHref !== prevUrl) 
-{
-          prevUrl = iframeLocationHref;
-          
-onIframeUrlChange(iframeLocationHref);
+window.addEventListener("message", (event) => {
+  if (
+    event.origin !== "https://dykarna.plingot.com" &&
+    event.origin !== "https://dykarna.plingot.com" &&
+    event.origin !== "https://dykarna.plingot.com" &&
+    event.origin !== "https://dykarna.plingot.com"
+  ) {
+    return;
+  }
 
-          if (typeof event.data.height === 
-"number") {
-            resizeIframe(event.data.height);
-          }
-        }
-      });
+  const iframeLocationHref = event.data.href;
+  if (iframeLocationHref !== prevUrl) {
+    prevUrl = iframeLocationHref;
 
-      function resizeIframe(height) {
-        var iframe = 
-document.getElementById("kalender");
-        iframe.style.height = height + 50 + 
-"px";
-      }
+    onIframeUrlChange(iframeLocationHref);
 
-      function extractValue(url) {
-        const startIndex = 
-url.indexOf("/event/") + 7;
-        const endIndex = url.indexOf("/", 
-startIndex);
+    if (typeof event.data.height === "number") {
+      resizeIframe(event.data.height);
+    }
+  }
+});
 
-        if (startIndex !== -1 && endIndex 
-=== -1) {
-          return url.substring(startIndex, 
-url.length);
-        } else if (startIndex !== -1 && 
-endIndex !== -1) {
-          return url.substring(startIndex, 
-endIndex);
-        }
+function resizeIframe(height) {
+  var iframe = document.getElementById("kalender");
+  iframe.style.height = height + 50 + "px";
+}
 
-        return "";
-      }
+function extractValue(url) {
+  const startIndex = url.indexOf("/event/") + 7;
+  const endIndex = url.indexOf("/", startIndex);
 
-      function onIframeUrlChange(newUrl) {
-        if (newUrl.includes("event/")) {
-          if (location.search === "") {
-            const eventId = 
-extractValue(newUrl);
-            
-window.history.replaceState(null, null, 
-`?eventId=${eventId}`);
-          }
-        } else {
-          if 
-(location.search.includes("?eventId=")) {
-            
-window.history.replaceState(null, null, 
-"/");
-          }
-        }
-      }
+  if (startIndex !== -1 && endIndex === -1) {
+    return url.substring(startIndex, url.length);
+  } else if (startIndex !== -1 && endIndex !== -1) {
+    return url.substring(startIndex, endIndex);
+  }
+
+  return "";
+}
+
+function onIframeUrlChange(newUrl) {
+  if (newUrl.includes("event/")) {
+    if (location.search === "") {
+      const eventId = extractValue(newUrl);
+
+      window.history.replaceState(null, null, `?eventId=${eventId}`);
+    }
+  } else {
+    if (location.search.includes("?eventId=")) {
+      window.history.replaceState(null, null, "/");
+    }
+  }
+}
